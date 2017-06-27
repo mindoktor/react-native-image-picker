4b3c50f0b2a34f3a5f64382ac215be241676056f
Konrad Beckmann <konrad.beckmann@mindoktor.se>
2017-06-27 13:47:26 +0200

    Clean up temporary images and videos properly

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -57,6 +57,8 @@ import static com.imagepicker.utils.MediaUtils.*;
 import static com.imagepicker.utils.MediaUtils.createNewFile;
 import static com.imagepicker.utils.MediaUtils.getResizedImage;
 
+// import android.util.Log;
+
 public class ImagePickerModule extends ReactContextBaseJavaModule
         implements ActivityEventListener
 {
@@ -384,7 +386,8 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
     // user cancel
     if (resultCode != Activity.RESULT_OK)
     {
-      removeUselessFiles(requestCode, imageConfig);
+      // Force cleanup of the files
+      removeUselessFiles(REQUEST_LAUNCH_IMAGE_CAPTURE, imageConfig);
       responseHelper.invokeCancel(callback);
       callback = null;
       return;
@@ -436,6 +439,8 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
         uri = cameraCaptureURI;
         updatedResultResponse(uri, imageConfig.original.getAbsolutePath());
         responseHelper.invokeResponse(callback);
+        callback = null;
+        this.options = null;
         return;
     }
 
@@ -473,6 +478,7 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
       }
       else
       {
+        removeOriginalFile(imageConfig);
         uri = Uri.fromFile(imageConfig.resized);
         BitmapFactory.decodeFile(imageConfig.resized.getAbsolutePath(), options);
         responseHelper.putInt("width", options.outWidth);
